513da68a51a5e1ffb4d8fb4a04969905eb0678ed
chrosslaw <Chrosslaw@gmail.com>
2023-01-22 16:55:51 -0600

    initial reddit and subreddit slice.

---
+++ src/features/reddits/redditsSlice.js
@@ -0,0 +1,9 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const redditsSlice = createSlice({
+  name: "reddits",
+  initialState: {
+    reddits: {},
+  },
+  reducers: {},
+});
---
+++ src/features/subreddits/subRedditsSlice.js
@@ -0,0 +1,9 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const subRedditsSlice = createSlice({
+  name: "subreddits",
+  initialState: {
+    reddits: {},
+  },
+  reducers: {},
+});
